7f716fa8c80989203b2b5d49be11b34902456d2e
Olivia Larson <olivialarson@gmail.com>
2014-03-31 22:36:23 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Physicalfocus::Application.config.secret_token = '5c7052edef89214e3bc3e9685e1bf653cb45bd81b758b58ec647ae9fd1145721d49d080aeadf06f4a0dd70b044044afc06ec18e498168008791cd912d8bffdf0'
